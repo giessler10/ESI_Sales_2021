8616ef4ddc0a3d3b40b6c20a5cee160f24b4b8b7
Tobias Giessler <tgiessle@stud.hs-offenburg.de>
2021-05-26 14:59:27 +0200

    Update lambda updateOrdersOrderitemState

--- updateOrdersOrderitemStatus/index.js
+++ updateOrdersOrderitemStatus/index.js
@@ -85,9 +85,22 @@ exports.handler = async (event, context, callback) => {
         context.fail(JSON.stringify(response));
       }
       else{
-        //Orders aktualisieren
-        await callDB(pool, updateOrdersOrderitemStatus(orders,IST_NR));
+        //Orderitems aktualisieren
         
+        var sql = buildSQLString(orders, IST_NR);
+        
+        
+        if(sql[0] != undefined){
+          await callDB(pool, sql[0]); //New oder Preproduction
+        }
+        if(sql[1] != undefined){
+          await callDB(pool, sql[1]); //Quality
+        }
+        
+        if(sql[2] != undefined){
+          await callDB(pool, sql[2]); //Item Return
+        }
+
         var messageJSON = {
           message: 'Der Status der Positionen der Bestellung wurde aktualisiert.'
         };
@@ -150,7 +163,6 @@ async function callDBResonse(client, queryMessage) {
 
 //************* Hilfsfunktionen *****************************
 
-
 const buildSQLWhereString = function (orders) {
   var where = "WHERE (OI_O_NR = " + orders[0]["O_NR"] + " and OI_NR = " + orders[0]["OI_NR"] + ")";
   
@@ -160,18 +172,71 @@ const buildSQLWhereString = function (orders) {
     } 
   }
 
-  return (where);
+  return where;
 };
+
+function buildSQLString(orders, IST_NR) {
+  var whereNew;
+  var whereQS;
+  var whereReturn;
+  
+  var queryMessageNew;
+  var queryMessageQS;
+  var queryMessageReturn;
+
+  for (var i = 0; i < orders.length; i++) {
+    //New (N) Orderitems or Preproduction (P)
+    if (orders[i]["PO_CODE"] == "P" || orders[i]["PO_CODE"] == "N"){
+      if (whereNew == undefined) {
+        whereNew = "where (OI_O_NR = " + orders[i]["O_NR"] + " and OI_NR = " + orders[i]["OI_NR"] + ")";
+      } 
+      else{
+       whereNew += " or (OI_O_NR = " + orders[i]["O_NR"] + " and OI_NR = " + orders[i]["OI_NR"] + ")";
+      }
+    }
+    
+    //Orderitems with Quality issues (Q)
+    if (orders[i]["PO_CODE"] == "Q"){
+      if (whereQS == undefined){
+        whereQS = "where (QI_O_NR = " + orders[i]["O_NR"] + " and QI_OI_NR = " + orders[i]["OI_NR"] + " and QI_COUNTER = " + orders[i]["PO_COUNTER"] + ")";
+      } 
+      else{
+       whereQS += " or (QI_O_NR = " + orders[i]["O_NR"] + " and QI_OI_NR = " + orders[i]["OI_NR"] + " and QI_COUNTER = '" + orders[i]["PO_COUNTER"] + ")";
+      }
+    } 
+    
+    //Orderitems that needs reproduction due to Return (R)
+    if (orders[i]["PO_CODE"] == "R"){
+      if (whereReturn == undefined){
+        whereReturn = "where (IR_O_NR = " + orders[i]["O_NR"] + " and IR_OI_NR = " + orders[i]["OI_NR"] + " and IR_COUNTER = " + orders[i]["PO_COUNTER"] + ")";
+      } 
+      else{
+        whereReturn += " or (IR_O_NR = " + orders[i]["O_NR"] + " and IR_OI_NR = " + orders[i]["OI_NR"] + " and IR_COUNTER = '" + orders[i]["PO_COUNTER"] + ")";
+      }
+    } 
+  }
+ 
+  if (whereNew != undefined) {
+    queryMessageNew = "UPDATE ORDER.ORDERITEM SET OI_IST_NR = " + IST_NR + " " + whereNew + "";
+  }
+ 
+  if (whereQS != undefined) {
+    queryMessageQS = "UPDATE QUALITY.QUALITYISSUE SET QI_IST_NR = " + IST_NR + " " + whereQS + "";
+  }
+  
+  if (whereReturn != undefined) {
+    queryMessageReturn = "UPDATE QUALITY.ITEMRETURN SET IR_IST_NR = " + " " +IST_NR + " " + whereReturn + "";
+  }
+  
+  console.log("R " + queryMessageReturn);
+  console.log("QS " + queryMessageQS);
+  console.log("N " + queryMessageNew);
+  
+  return [queryMessageNew,queryMessageQS,queryMessageReturn];
+}
 
 
 //******* SQL Statements *******
-
-const updateOrdersOrderitemStatus = function (orders, IST_NR) {
-  var where = buildSQLWhereString(orders); //WHERE Statement
-  var queryMessage = "UPDATE ORDER.ORDERITEM SET OI_IST_NR = " + IST_NR + " " + where + ";";
-  //console.log(queryMessage);
-  return (queryMessage);
-};
 
 const checkOrdersExist = function (orders) {
   var where = buildSQLWhereString(orders); //WHERE Statement
